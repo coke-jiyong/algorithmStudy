6b93a782df019d9da4b524b41109998897c55437
wldyddjq <jypark@ssenstone.com>
2025-03-27 10:00:59 +0000

    2025-03-27

--- 9017/9017.cpp
+++ 9017/9017.cpp
@@ -1,36 +1,77 @@
 #include <iostream>
-
+#include <map>
+#include <set>
+#include <vector>
 using namespace std;
 int N;
+
+int findMinTeam(const std::map<int, std::set<int>> &data)
+{
+	int min = 2147483646;
+	int minFifth = 2147483646;
+	int resultTeam = -1;
+	for (auto &[team, scores] : data)
+	{
+		int sum = 0;
+		auto itr = scores.begin();
+
+		sum += *itr++;
+		sum += *itr++;
+		sum += *itr++;
+		sum += *itr++;
+
+		int fifth = *itr;
+		if (sum < min)
+		{
+			min = sum;
+			minFifth = fifth;
+			resultTeam = team;
+		}
+		else if (sum == min && minFifth > fifth)
+		{
+			minFifth = fifth;
+			resultTeam = team;
+		}
+	}
+
+	return resultTeam;
+}
+// map<팀명 ,완주한 사람 수>
+// 입력되는 순서대로 일단 입력.
+
 int main(void)
 {
-    int T;
-    cin >> T;
+	int T;
+	cin >> T;
 
-    int teams[200] = {0};
-    int ranks[1000];
-    for (int i = 0; i < T; i++)
-    {
-        cin >> N;
-        for (int k = 0; k < N; k++)
-        {
-            int l;
-            cin >> l;
-            ranks[k] = l;
-            teams[l] += 1;
-        }
-    }
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+		vector<int> input;
+		map<int, int> team;
+		for (int k = 0; k < N; k++)
+		{
+			int teamName;
+			cin >> teamName;
+			team[teamName]++;
+			input.push_back(teamName);
+		}
 
-    for (int i = 1; i < 200; i++)
-    {
-        if (teams[i] == 0)
-            break;
+		int score = 1;
+		map<int, set<int>> validTeam;
+		for (auto &name : input)
+		{
+			if (team[name] == 6)
+			{
+				validTeam[name].insert(score++);
+			}
+			else
+			{
+				continue;
+			}
+		}
 
-        if (teams[i] < 6)
-        {
-            teams[i] = 1001;
-        }
-    }
-
-    return 0;
+		cout << findMinTeam(validTeam) << endl;
+	}
+	return 0;
 }
