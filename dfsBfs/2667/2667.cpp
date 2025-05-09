ee1fd9567eb2a38fc54a4428f571e2c0d3e22668
coke-jiyong <qkrwldyd488@naver.com>
2025-05-09 02:36:48 +0000

    2667성공

--- dfsBfs/2667/2667.cpp
+++ dfsBfs/2667/2667.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int map[25][25];
-bool visited[25][25];
-pair<int, int> way[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 위오아왼
+bool visited[25][25]; //     ↑       →       ↓       ←
+pair<int, int> way[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 int cnt;
 int N;
 void dfs(int i, int j)
 {
-    visited[i][j] = true;
-    for (int o = 0; o < 4; o++) // 위오아왼 확인
+    visited[i][j] = true;       // 방문한 노드 visited 등록
+    for (int o = 0; o < 4; o++) // 동서남북 방향 확인
     {
-        int row = i + way[o].first;
-        int col = j + way[o].second;
-        if (row < 0 || row >= N || col < 0 || col >= N)
+        int row = i + way[o].first;  // 행 연산
+        int col = j + way[o].second; // 열 연산
+
+        if (row < 0 || row >= N || col < 0 || col >= N) // 버퍼 오버플로우 방지
             continue;
 
-        if (map[row][col] == 1 && !visited[row][col])
+        if (map[row][col] == 1 && !visited[row][col]) // 동서남북 방향 중 집이 존재하고, 방문하지 않은 인덱스면 cnt + 1 후 재귀호출
         {
             cnt++;
             dfs(row, col);
         }
     }
 }
+
 int main()
 {
+    vector<int> answer;
     cin >> N;
     for (int i = 0; i < N; i++)
     {
-        string str;
+        string str; // 입력 주의, 정수로 입력받아봤지만 안됨 : 정수로 입력받으려면 0 1 1 0 이런식으로 스페이스 한칸이 필요 함
         cin >> str;
         for (int j = 0; j < str.length(); j++)
         {
@@ -42,13 +47,18 @@ int main()
     {
         for (int j = 0; j < N; j++)
         {
-            if (!visited[i][j] && map[i][j] == 1)
+            if (!visited[i][j] && map[i][j] == 1) // 모든 인덱스를 탐색하며 방문하지 않았고, 집이 있다면 cnt 1 로 초기화 후 재귀 시작
             {
                 cnt = 1;
                 dfs(i, j);
-                cout << cnt << endl;
+                // cout << cnt << endl;
+                answer.push_back(cnt);
             }
         }
     }
+    sort(answer.begin(), answer.end());
+    cout << answer.size() << endl;
+    for (auto &i : answer)
+        cout << i << endl;
     return 0;
 }
