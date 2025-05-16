14003e97e12bfe0eed374492c6bcd8eaa0aff360
DESKTOP-3N56K9C\LG <jypark@ssenstone.com>
2025-05-16 09:00:44 +0000

    2146...

--- dfsBfs/2146/2146.cpp
+++ dfsBfs/2146/2146.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <queue>
 #include <cstring>
-#include <set>
-#include <tuple>
+#include <vector>
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 using namespace std;
-
 //                          ↑        →       ↓       ←
 pair<int, int> way[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 // 각 섬마다 번호붙인 후 가장자리 큐에 삽입
 int ground[101][101];
 bool visited[101][101];
-set<pair<int, int>> edges;
-
+vector<pair<int, int>> edges[100001]; // 섬의 태그는 101을 초과 할 수 있음 . .. .
 int N;
 void input()
 {
@@ -42,6 +40,7 @@ void name_tag_land(int r, int c, int tag)
             int newC = col + way[i].second;
             if (newR < 0 || newC < 0 || newR >= N || newC >= N)
                 continue;
+
             if (!visited[newR][newC] && ground[newR][newC] == 1)
             {
                 q.push({newR, newC});
@@ -50,48 +49,48 @@ void name_tag_land(int r, int c, int tag)
             else if (ground[newR][newC] == 0 && is_edge == false)
                 is_edge = true;
         }
-        if (is_edge)
-            edges.insert({row, col});
-    }
-}
-void print()
-{
-    cout << "ground : " << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            cout << ground[i][j] << ' ';
-        cout << "\n";
-    }
-    cout << "edges : " << endl;
-    for (auto &i : edges)
-        cout << i.first << "," << i.second << endl;
-}
 
-int bfs_from_edge()
+        if (is_edge)
+            edges[tag].push_back({row, col});
+    }
+}
+struct info
+{
+    int row;
+    int col;
+    int cnt;
+    int island;
+} INFO;
+int bfs_from_edge(int tag)
 {
     memset(visited, false, sizeof(visited));
-    queue<tuple<int, int, int, int>> q;
-    for (auto &i : edges)
-        q.push({i.first, i.second, 0, ground[i.first][i.second]});
+    queue<info> q;
+    // 0 = edgesRow , 1 = edgesCol , 2 = count , 3 = islandNum
+    for (auto &i : edges[tag])
+    {
+        q.push(info{i.first, i.second, 0, ground[i.first][i.second]});
+        visited[i.first][i.second] = true; // 시작점도 방문처리! 집중하자
+    }
+
     while (!q.empty())
     {
         auto t = q.front();
         q.pop();
-        int r = get<0>(t);
-        int c = get<1>(t);
-
+        int r = t.row;
+        int c = t.col;
         for (int i = 0; i < 4; i++)
         {
             int newR = r + way[i].first;
             int newC = c + way[i].second;
             if (newR < 0 || newC < 0 || newR >= N || newC >= N)
                 continue;
-            if (ground[newR][newC] != 0 && ground[newR][newC] != get<3>(t))
-                return get<2>(t);
+
+            if (ground[newR][newC] != 0 && ground[newR][newC] != t.island)
+                return t.cnt;
+
             if (!visited[newR][newC] && ground[newR][newC] == 0)
             {
-                q.push({newR, newC, get<2>(t) + 1, ground[newR][newC]});
+                q.push(info{newR, newC, t.cnt + 1, t.island}); // 현재 섬의 태그를 갱신시키지말고 그대로 넣어줘야 함.
                 visited[newR][newC] = true;
             }
         }
@@ -112,7 +111,10 @@ void solution()
             }
         }
     }
-    int res = bfs_from_edge();
+    int res = 1e9;
+    for (int i = 2; i < tag; i++)
+        res = MIN(res, bfs_from_edge(i));
+
     cout << res << endl;
 }
 
@@ -121,8 +123,6 @@ int main(void)
     input();
 
     solution();
-
-    print();
 
     return 0;
 }
