1f4aebd7ac46b6ecfe368fcc19f998983929536f
DESKTOP-3N56K9C\LG <jypark@ssenstone.com>
2025-06-02 08:35:16 +0000

    2447이해불가

---
+++ 2447/2447.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int N;
+void print_basic()
+{
+    cout << "***\
+             * *\
+             ***";
+}
+void input()
+{
+    cin >> N;
+}
+
+void sol()
+{
+}
+int main(void)
+{
+    print_basic();
+    print_basic();
+    // print_basic();
+    // cout << endl;
+    // print_basic();
+    // cout << "   " << endl;
+    // print_basic();
+    return 0;
+}
